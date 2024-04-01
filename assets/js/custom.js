780ceb962ce5995854090ed58cfbb0a7dd4a6c46
HenaPot <hena.potogija@stu.ibu.edu.ba>
2024-04-01 15:14:11 +0200

    ajax 2

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
   });
 
 $(document).ready(function() {
-    // Use jQuery AJAX to fetch JSON data from the file - SLIDER
+    // Use jQuery AJAX to fetch JSON data from the file - SLIDER PARKING LOT
     $.ajax({
         url: 'json/parking_lots.json',
         dataType: 'json',
@@ -45,5 +45,36 @@ $(document).ready(function() {
     });
 
 });
+
+$(document).ready(function() {
+    // Use jQuery AJAX to fetch JSON data from the file - SLIDER PRICING
+    $.ajax({
+        url: 'json/pricing.json',
+        dataType: 'json',
+        success: function(data) {
+            //console.log('you are here !');
+            // Iterate over each item in the JSON array
+            $.each(data, function(index, item) {
+                // Create option element with attributes
+                var option = $('<option>', {
+                    value: item.value,
+                    text: item.text,
+                    // Add additional attributes here
+                    class: 'input-group', // Example: Add a class
+                    'data-display': item.text, // Example: Add a data attribute
+                });
+                
+                // Append the option to the select element
+                $('#select-pricing').append(option);
+            });
+        },
+        error: function(xhr, status, error) {
+            console.error('Error loading JSON data:', error);
+        }
+    });
+
+});
+
+
 
 
